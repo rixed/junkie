b2781c4739caacc987b4b30512d01ac1d619af66
Cedric Cellier <cedric.cellier@securactive.net>
2011-06-06 16:10:59 +0200

    chg: lower log level of msg informing that a capfile cannot be opened due to user limit

--- src/capfile.c
+++ src/capfile.c
@@ -142,7 +142,7 @@ static void capfile_close(struct capfile *capfile)
 static int capfile_open(struct capfile *capfile, char const *path)
 {
     if (capture_files >= max_capture_files) {
-        SLOG(LOG_WARNING, "Cannot open new capture files: %u already opened", capture_files);
+        SLOG(LOG_INFO, "Cannot open new capture files: %u already opened", capture_files);
         return -1;
     }
 
