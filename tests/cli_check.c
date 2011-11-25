a8d78dba136f3164fd9a78d674a3992fbf791671
Cedric Cellier <cedric.cellier@securactive.net>
2011-11-25 13:39:29 +0100

    new: add the CLI_SET_ENUM type of command line parameters

--- tests/cli_check.c
+++ tests/cli_check.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #undef NDEBUG
 #include <assert.h>
+#include <string.h>
 #include "tools/cli.c"
 
 static void check_cli_2_enum(void)
@@ -16,6 +17,20 @@ static void check_cli_2_enum(void)
     assert( 1 == cli_2_enum(false, "GLOP", "pas glop", "glop", NULL));
 }
 
+static void check_enum_2_str(void)
+{
+    char const *values = "glop|glop glop|pas glop";
+    assert(NULL == cli_enum_2_str(10, values));
+    assert(NULL == cli_enum_2_str(3, values));
+    assert(NULL == cli_enum_2_str(1, ""));
+    assert(0 == strcmp(cli_enum_2_str(0, values), "glop"));
+    assert(0 == strcmp(cli_enum_2_str(1, values), "glop glop"));
+    assert(0 == strcmp(cli_enum_2_str(2, values), "pas glop"));
+    assert(0 == strcmp(cli_enum_2_str(0, "0|1|2"), "0"));
+    assert(0 == strcmp(cli_enum_2_str(1, "0|1|2"), "1"));
+    assert(0 == strcmp(cli_enum_2_str(2, "0|1|2"), "2"));
+}
+
 int main(void)
 {
     log_init();
@@ -23,6 +38,7 @@ int main(void)
     log_set_file("cli_check.log");
 
     check_cli_2_enum();
+    check_enum_2_str();
 
     log_fini();
     return EXIT_SUCCESS;
