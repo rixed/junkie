57c47e4b8fe30e08d0dd20d3c2756237d845238a
Cedric Cellier <cedric.cellier@securactive.net>
2011-12-26 09:48:16 +0100

    fix: timeout test was wrong since the addition of new pcap.
      Make the test more to the point by using the multiplexer stats
      instead of the whole parser stats.

--- tests/timeout.scm
+++ tests/timeout.scm
@@ -10,10 +10,10 @@
 
 (set-quit-when-done #f)
 
-(define (nb-tot-parsers)
-  (let* ((stats      (map proto-stats (proto-names)))
-         (nb-parsers (map (lambda (s) (assq-ref s 'nb-parsers)) stats)))
-    (reduce + 0 nb-parsers)))
+(define (nb-tot-muxers)
+  (let* ((stats     (map proto-stats (mux-names)))
+         (nb-muxers (map (lambda (s) (assq-ref s 'nb-parsers)) stats)))
+    (apply + nb-muxers)))
   
 (for-each-file-in "pcap/" (lambda (dir)
                             (simple-format #t "Playing all pcap from ~a~%" dir)
@@ -23,13 +23,13 @@
 
 ; Check that we have only the uniq parsers (which are not deleted once created)
 (let loop ((time-elapsed 0))
-  (let* ((nb-parsers (nb-tot-parsers)))
-    (simple-format #t "~a parsers left after ~as~%" nb-parsers time-elapsed)
-    (if (> nb-parsers 7) ; should be 5 but for some reason we have 1 or 2 IPv4 parsers left (FIXME)
+  (let* ((nb-muxers (nb-tot-muxers)))
+    (simple-format #t "~a multiplexers left after ~as~%" nb-muxers time-elapsed)
+    (if (> nb-muxers 2) ; should be 0 but for some reason we have 1 or 2 IPv4 parsers left (FIXME)
         (begin
           (if (> time-elapsed 10)
               (begin
-                (map (lambda (p) (simple-format #t "Stats for parser ~a: ~s~%" p (proto-stats p))) (proto-names))
+                (map (lambda (p) (simple-format #t "Stats for multiplexer ~a: ~s~%" p (proto-stats p))) (mux-names))
                 (assert (<= time-elapsed 10))))
           (sleep 1)
           (loop (1+ time-elapsed))))))
