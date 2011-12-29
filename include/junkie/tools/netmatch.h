5c84a36793f97eb6f8ebbf7a7673e71a3b00c8a5
Cedric Cellier <cedric.cellier@securactive.net>
2011-12-29 18:50:23 +0100

    new: preview of what's to be done

---
+++ include/junkie/tools/netmatch.h
@@ -0,0 +1,13 @@
+// -*- c-basic-offset: 4; c-backslash-column: 79; indent-tabs-mode: nil -*-
+// vim:sw=4 ts=4 sts=4 expandtab
+#ifndef NETMATCH_H_111229
+#define NETMATCH_H_111229
+
+#include <stdint.h>
+
+struct npc_register {
+    intptr_t value;
+    size_t size;
+};
+
+#endif
