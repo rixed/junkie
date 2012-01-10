aeb868c8e86d6d98402553805c7c66b424bc1fbb
Cedric Cellier <cedric.cellier@securactive.net>
2012-01-10 17:34:18 +0100

    fix: ARP now uses the same type for MAC than proto eth.

--- include/junkie/proto/arp.h
+++ include/junkie/proto/arp.h
@@ -2,7 +2,6 @@
 // vim:sw=4 ts=4 sts=4 expandtab
 #ifndef ARP_H_100402
 #define ARP_H_100402
-#include <stdint.h>
 #include <junkie/proto/proto.h>
 #include <junkie/proto/eth.h>   // for ETH_ADDR_LEN
 #include <junkie/tools/ip_addr.h>
@@ -15,13 +14,13 @@ extern struct proto *proto_arp;
 
 /// Description of an ARP message
 struct arp_proto_info {
-    struct proto_info info;             ///< Generic infos
+    struct proto_info info;                 ///< Generic infos
     enum arp_opcode { ARP_REQUEST=1, ARP_REPLY=2 } opcode;
     bool proto_addr_is_ip;
     bool hw_addr_is_eth;
-    struct ip_addr sender;              ///< Set iff proto_addr_is_ip
-    struct ip_addr target;              ///< Set iff proto_addr_is_ip
-    uint8_t hw_target[ETH_ADDR_LEN];    ///< Set iff opcode == 2
+    struct ip_addr sender;                  ///< Set iff proto_addr_is_ip
+    struct ip_addr target;                  ///< Set iff proto_addr_is_ip
+    unsigned char hw_target[ETH_ADDR_LEN];  ///< Set iff opcode == 2
 };
 
 void arp_init(void);
