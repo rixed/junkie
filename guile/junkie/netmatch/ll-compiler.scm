0ed9fad55186d8db7ede43daffe292e100f30d9d
Cedric Cellier <rixed@happyleptic.org>
2012-07-31 15:00:06 +0200

    fix: netmatch compiler was including non-existant headers
      ie. bittorrent.h and ssl.h

--- guile/junkie/netmatch/ll-compiler.scm
+++ guile/junkie/netmatch/ll-compiler.scm
@@ -13,7 +13,7 @@
 
 ; FIXME: instead of this, an alist of protos (records) giving the header name, the fields, etc...
 (define *all-protos*
-  '(cap eth ip gre arp udp icmp tcp sip bittorrent http rtp netbios ssl dns rtcp ftp mgcp sdp sql))
+  '(cap eth ip gre arp udp icmp tcp sip http rtp netbios dns rtcp ftp mgcp sdp sql))
 
 (define (headers-for proto-list)
   ; TODO : uniquifies then map to include stenzas
