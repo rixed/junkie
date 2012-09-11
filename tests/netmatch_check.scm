7356b159664d16b5901b87ed3ff866ecb378fb12
Cedric Cellier <rixed@happyleptic.org>
2012-09-11 17:39:58 +0200

    fix: many bugs in IP and subnet types

--- tests/netmatch_check.scm
+++ tests/netmatch_check.scm
@@ -4,9 +4,14 @@
 
 (define-module (check))
 (use-modules ((junkie netmatch nettrack) :renamer (symbol-prefix-proc 'nt:))
+             ((junkie netmatch types) :renamer (symbol-prefix-proc 'type:))
              (junkie runtime)
              (junkie tools)
              (junkie defs))
+
+(display "Testing netmatch tools\n")
+
+(assert (type:looks-like-subnet? '192.168.10.0/255.255.255.0))
 
 (display "Testing netmatch expressions\n")
 
