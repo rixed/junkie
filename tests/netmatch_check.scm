563a140b99c49b40562afcc14021bcca3a00c2b3
Cedric Cellier <rixed@happyleptic.org>
2012-09-19 11:09:14 +0200

    new: a test against an imaginary bug

--- tests/netmatch_check.scm
+++ tests/netmatch_check.scm
@@ -134,5 +134,17 @@
 
 (assert (= called 23))
 
+; Test eager evaluation of or
+
+(test "eager eval of or"
+      '([]
+        [(node
+           (on-entry (apply (check) incr-called)))]
+        [(root node
+            (match (tcp) (or #t #f #f)) ; make certain that ors are composed correctly
+            spawn)]))
+
+(assert (>= called 54)) ; although there are only 54 packets we may be called more than that due to the reconstruction of HTTP messages
+
 ;; good enough!
 (exit 0)
