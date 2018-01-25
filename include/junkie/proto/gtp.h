47eb2925c7a828fd28cc2d933f767d69e6a81bce
Cedric Cellier <rixed@happyleptic.org>
2018-01-25 08:22:01 +0100

    GTP: skip extensions, identifies msg types, use cursor.

--- include/junkie/proto/gtp.h
+++ include/junkie/proto/gtp.h
@@ -10,17 +10,65 @@
 
 extern struct proto *proto_gtp;
 
+enum gtp_msg_type {
+#   define RR(name, n) \
+    GTP_##name##_REQ = n, \
+    GTP_##name##_RESP = (n+1)
+
+    RR(ECHO, 1),
+    GTP_VERSION_NOT_SUPPORTED = 3,
+    RR(NODE_ALIVE, 4),
+    RR(REDIRECTION, 6),
+    RR(CREATE_PDP_CONTEXT, 16),
+    RR(UPDATE_PDP_CONTEXT, 18),
+    RR(DELETE_PDP_CONTEXT, 20),
+    RR(INITIATE_PDP_CONTEXT_ACTIV, 22),
+    GTP_ERROR_INDIC = 26,
+    RR(PDU_NOTIF, 27),
+    RR(PDU_NOTIF_REJECT, 29),
+    GTP_SUPPORTED_EXTENSIONS_NOTIF = 31,
+    RR(SEND_ROUTING_INFO, 32),
+    RR(FAILURE_REPORT, 34),
+    RR(MS_PRESENT, 36),
+    RR(IDENTIFICATION, 48),
+    RR(SGSN_CONTEXT, 50),
+    GTP_SGSN_CONTEXT_ACK = 52,
+    RR(FORWARD_RELOC, 53),
+    GTP_FORWARD_RELOC_COMPLETE = 55,
+    RR(RELOC_CANCEL, 56),
+    GTP_FORWARD_SRNS_CONTEXT = 58,
+    GTP_FORWARD_RELOC_COMPLETE_ACK = 59,
+    GTP_FORWARD_SRNS_CONTEXT_ACK = 60,
+    RR(UE_REGISTRATION_QUERY, 61),
+    GTP_RAN_INFO_RELAY = 70,
+    RR(MBMS_NOTIF, 96),
+    RR(MBMS_NOTIF_REJECT, 98),
+    RR(CREATE_MBMS_CONTEXT, 100),
+    RR(UPDATE_MBMS_CONTEXT, 102),
+    RR(DELETE_MBMS_CONTEXT, 104),
+    RR(MBMS_REGISTRATION, 112),
+    RR(MBMS_DEREGISTRATION, 114),
+    RR(MBMS_SESSION_START, 116),
+    RR(MBMS_SESSION_STOP, 118),
+    RR(MBMS_SESSION_UPDATE, 120),
+    RR(MS_INFO_CHG_NOTIF, 128),
+    RR(DATA_RECORD_TRANSFERT, 240),
+    // TODO: 3GPP TS 29.281 [41]
+    GTP_END_MARKER = 254,
+    GTP_GPDU = 255
+};
+
 /// GTP message
 struct gtp_proto_info {
     struct proto_info info;
-    unsigned version:3;
-    unsigned msg_type:8;
+    enum gtp_msg_type msg_type;
 #   define GTP_HAS_TEID        0x0001  // GTPv2 might have no TEID
 #   define GTP_HAS_SEQNUM      0x0002
 #   define GTP_HAS_NPDU_NUMBER 0x0004
     unsigned set_values;
     uint32_t teid;
     uint16_t seqnum;
+    uint8_t version;
     uint8_t npdu_number;
 };
 
--- src/proto/gtp.c
+++ src/proto/gtp.c
@@ -18,8 +18,8 @@
  * along with Junkie.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <assert.h>
-#include <arpa/inet.h>  // for ntohl
 #include "junkie/tools/objalloc.h"
+#include "junkie/proto/cursor.h"
 #include "junkie/proto/udp.h"
 #include "junkie/proto/gtp.h"
 
@@ -77,13 +77,89 @@ static void const *gtp_info_addr(struct proto_info const *info_, size_t *size)
     return info;
 }
 
+static char const *msg_type_2_str(enum gtp_msg_type t)
+{
+    switch (t) {
+        case GTP_ECHO_REQ: return "ECHO REQUEST";
+        case GTP_ECHO_RESP: return "ECHO RESPONSE";
+        case GTP_VERSION_NOT_SUPPORTED: return "VERSION NOT SUPPORTED";
+        case GTP_NODE_ALIVE_REQ: return "NODE ALIVE REQUEST";
+        case GTP_NODE_ALIVE_RESP: return "NODE ALIVE RESPONSE";
+        case GTP_REDIRECTION_REQ: return "REDIRECTION REQUEST";
+        case GTP_REDIRECTION_RESP: return "REDIRECTION RESPONSE";
+        case GTP_CREATE_PDP_CONTEXT_REQ: return "CREATE PDP CONTEXT REQUEST";
+        case GTP_CREATE_PDP_CONTEXT_RESP: return "CREATE PDP CONTEXT RESPONSE";
+        case GTP_UPDATE_PDP_CONTEXT_REQ: return "UPDATE PDP CONTEXT REQUEST";
+        case GTP_UPDATE_PDP_CONTEXT_RESP: return "UPDATE PDP CONTEXT RESPONSE";
+        case GTP_DELETE_PDP_CONTEXT_REQ: return "DELETE PDP CONTEXT REQUEST";
+        case GTP_DELETE_PDP_CONTEXT_RESP: return "DELETE PDP CONTEXT RESPONSE";
+        case GTP_INITIATE_PDP_CONTEXT_ACTIV_REQ: return "INITIATE PDP CONTEXT ACTIV REQUEST";
+        case GTP_INITIATE_PDP_CONTEXT_ACTIV_RESP: return "INITIATE PDP CONTEXT ACTIV RESPONSE";
+        case GTP_ERROR_INDIC: return "ERROR INDIC";
+        case GTP_PDU_NOTIF_REQ: return "PDU NOTIF REQUEST";
+        case GTP_PDU_NOTIF_RESP: return "PDU NOTIF RESPONSE";
+        case GTP_PDU_NOTIF_REJECT_REQ: return "PDU NOTIF REJECT REQUEST";
+        case GTP_PDU_NOTIF_REJECT_RESP: return "PDU NOTIF REJECT RESPONSE";
+        case GTP_SUPPORTED_EXTENSIONS_NOTIF: return "SUPPORTED EXTENSIONS NOTIF";
+        case GTP_SEND_ROUTING_INFO_REQ: return "SEND ROUTING INFO REQUEST";
+        case GTP_SEND_ROUTING_INFO_RESP: return "SEND ROUTING INFO RESPONSE";
+        case GTP_FAILURE_REPORT_REQ: return "FAILURE REPORT REQUEST";
+        case GTP_FAILURE_REPORT_RESP: return "FAILURE REPORT RESPONSE";
+        case GTP_MS_PRESENT_REQ: return "MS PRESENT REQUEST";
+        case GTP_MS_PRESENT_RESP: return "MS PRESENT RESPONSE";
+        case GTP_IDENTIFICATION_REQ: return "IDENTIFICATION REQUEST";
+        case GTP_IDENTIFICATION_RESP: return "IDENTIFICATION RESPONSE";
+        case GTP_SGSN_CONTEXT_REQ: return "SGSN CONTEXT REQUEST";
+        case GTP_SGSN_CONTEXT_RESP: return "SGSN CONTEXT RESPONSE";
+        case GTP_SGSN_CONTEXT_ACK: return "SGSN CONTEXT ACK";
+        case GTP_FORWARD_RELOC_REQ: return "FORWARD RELOC REQUEST";
+        case GTP_FORWARD_RELOC_RESP: return "FORWARD RELOC RESPONSE";
+        case GTP_FORWARD_RELOC_COMPLETE: return "FORWARD RELOC COMPLETE";
+        case GTP_RELOC_CANCEL_REQ: return "RELOC CANCEL REQUEST";
+        case GTP_RELOC_CANCEL_RESP: return "RELOC CANCEL RESPONSE";
+        case GTP_FORWARD_SRNS_CONTEXT: return "FORWARD SRNS CONTEXT";
+        case GTP_FORWARD_RELOC_COMPLETE_ACK: return "FORWARD RELOC COMPLETE ACK";
+        case GTP_FORWARD_SRNS_CONTEXT_ACK: return "FORWARD SRNS CONTEXT ACK";
+        case GTP_UE_REGISTRATION_QUERY_REQ: return "UE REGISTRATION QUERY REQUEST";
+        case GTP_UE_REGISTRATION_QUERY_RESP: return "UE REGISTRATION QUERY RESPONSE";
+        case GTP_RAN_INFO_RELAY: return "RAN INFO RELAY";
+        case GTP_MBMS_NOTIF_REQ: return "MBMS NOTIF REQUEST";
+        case GTP_MBMS_NOTIF_RESP: return "MBMS NOTIF RESPONSE";
+        case GTP_MBMS_NOTIF_REJECT_REQ: return "MBMS NOTIF REJECT REQUEST";
+        case GTP_MBMS_NOTIF_REJECT_RESP: return "MBMS NOTIF REJECT RESPONSE";
+        case GTP_CREATE_MBMS_CONTEXT_REQ: return "CREATE MBMS CONTEXT REQUEST";
+        case GTP_CREATE_MBMS_CONTEXT_RESP: return "CREATE MBMS CONTEXT RESPONSE";
+        case GTP_UPDATE_MBMS_CONTEXT_REQ: return "UPDATE MBMS CONTEXT REQUEST";
+        case GTP_UPDATE_MBMS_CONTEXT_RESP: return "UPDATE MBMS CONTEXT RESPONSE";
+        case GTP_DELETE_MBMS_CONTEXT_REQ: return "DELETE MBMS CONTEXT REQUEST";
+        case GTP_DELETE_MBMS_CONTEXT_RESP: return "DELETE MBMS CONTEXT RESPONSE";
+        case GTP_MBMS_REGISTRATION_REQ: return "MBMS REGISTRATION REQUEST";
+        case GTP_MBMS_REGISTRATION_RESP: return "MBMS REGISTRATION RESPONSE";
+        case GTP_MBMS_DEREGISTRATION_REQ: return "MBMS DEREGISTRATION REQUEST";
+        case GTP_MBMS_DEREGISTRATION_RESP: return "MBMS DEREGISTRATION RESPONSE";
+        case GTP_MBMS_SESSION_START_REQ: return "MBMS SESSION START REQUEST";
+        case GTP_MBMS_SESSION_START_RESP: return "MBMS SESSION START RESPONSE";
+        case GTP_MBMS_SESSION_STOP_REQ: return "MBMS SESSION STOP REQUEST";
+        case GTP_MBMS_SESSION_STOP_RESP: return "MBMS SESSION STOP RESPONSE";
+        case GTP_MBMS_SESSION_UPDATE_REQ: return "MBMS SESSION UPDATE REQUEST";
+        case GTP_MBMS_SESSION_UPDATE_RESP: return "MBMS SESSION UPDATE RESPONSE";
+        case GTP_MS_INFO_CHG_NOTIF_REQ: return "MS INFO CHG NOTIF REQUEST";
+        case GTP_MS_INFO_CHG_NOTIF_RESP: return "MS INFO CHG NOTIF RESPONSE";
+        case GTP_DATA_RECORD_TRANSFERT_REQ: return "DATA RECORD TRANSFERT REQUEST";
+        case GTP_DATA_RECORD_TRANSFERT_RESP: return "DATA RECORD TRANSFERT RESPONSE";
+        case GTP_END_MARKER: return "END MARKER";
+        case GTP_GPDU: return "G-PDU";
+        default: return tempstr_printf("Unknown GTP message type %d", t);
+    }
+}
+
 static char const *gtp_info_2_str(struct proto_info const *info_)
 {
     struct gtp_proto_info const unused_ *info = DOWNCAST(info_, info, gtp_proto_info);
-    return tempstr_printf("%s, Version:%d, MsgType:%d%s%s%s",
+    return tempstr_printf("%s, Version:%u, MsgType:%s%s%s%s",
         proto_info_2_str(info_),
         info->version,
-        info->msg_type,
+        msg_type_2_str(info->msg_type),
         info->set_values & GTP_HAS_TEID ? tempstr_printf(", TEID:%"PRIu32, info->teid) : "",
         info->set_values & GTP_HAS_SEQNUM ? tempstr_printf(", SeqNum:%"PRIu16, info->seqnum) : "",
         info->set_values & GTP_HAS_NPDU_NUMBER ? tempstr_printf(", N-PDU:%"PRIu8, info->npdu_number) : "");
@@ -101,9 +177,12 @@ static void gtp_proto_info_ctor(struct gtp_proto_info *info, struct parser *pars
 
 static enum proto_parse_status gtp_parse_version1(struct gtp_proto_info *info, uint8_t const *packet, size_t cap_len, size_t unused_ wire_len)
 {
-    assert(cap_len >= 8);
+    struct cursor cur;
+    cursor_ctor(&cur, packet, cap_len);
+#   define CHECK(n) CHECK_LEN(&cur, n, 0)
+    CHECK(8);
 
-    uint8_t flags = packet[0];
+    uint8_t flags = cursor_read_u8(&cur);
 
     // We do not care about GTP':
     if (!(flags & 0x10)) {
@@ -111,16 +190,46 @@ static enum proto_parse_status gtp_parse_version1(struct gtp_proto_info *info, u
         return PROTO_PARSE_ERR;
     }
 
-    bool unused_ has_extension = flags & 0x04;
+    bool has_extension = flags & 0x04;
     info->set_values |= GTP_HAS_TEID |
                         (flags & 0x02 ? GTP_HAS_SEQNUM : 0) |
                         (flags & 0x01 ? GTP_HAS_NPDU_NUMBER : 0);
-    info->msg_type = packet[1];
+    info->msg_type = cursor_read_u8(&cur);
+    unsigned msg_length = cursor_read_u16n(&cur);
+    info->teid = cursor_read_u32n(&cur);
+    // After that starts the message which length is msg_length
+    uint8_t const *msg_start = cur.head;
 
-    unsigned unused_ msg_length = ((unsigned)packet[2] << 8) + packet[3];
+    enum gtp_extension_type {
+        GTP_EXT_NO_MORE = 0x00,
+        GTP_EXT_MBMS_SUPPORT_INDIC = 0x01,
+        GTP_EXT_MSINFO_CHG_REP_SUPPORT_INDIC = 0x02,
+        // TODO: Check 3GPP TS 29.281 [41]
+        GTP_EXT_PDCT_PDU_NUMBER = 0xC0,
+        GTP_EXT_SUSPEND_REQ = 0xC1,
+        GTP_EXT_SUSPEND_RESP = 0xC2,
+    } next_header_type = GTP_EXT_NO_MORE;
 
-    memcpy(&info->teid, packet+4, sizeof(info->teid));
-    info->teid = ntohl(info->teid);
+    if (flags & 7) {
+        CHECK(4);
+        info->seqnum = info->set_values & GTP_HAS_SEQNUM ?
+            cursor_read_u16n(&cur) : 0;
+        info->npdu_number = info->set_values & GTP_HAS_NPDU_NUMBER ?
+            cursor_read_u8(&cur) : 0;
+        next_header_type = has_extension ? cursor_read_u8(&cur) : 0;
+    }
+
+    // Skip headers
+    while (next_header_type != GTP_EXT_NO_MORE) {
+        CHECK(4);
+        size_t len = *cur.head << 2U;
+        CHECK(len);
+        next_header_type = cur.head[len-1];
+        cursor_drop(&cur, len);
+    }
+
+    msg_length -= cur.head - msg_start;
+
 
     return PROTO_OK;
 }
